fd719b458bfe8d068b8fd2df1b2d71e5efabd810
zinnoberHaus <zinnober.haus@gmail.com>
2023-05-11 10:45:49 -0400

    updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.write('Hello world!')
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=["a", "b", "c"])
+
+st.bar_chart(chart_data)
