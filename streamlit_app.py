19df2b3037929ca5c70d2e83f1226a62494d78d8
zinnoberHaus <zinnober.haus@gmail.com>
2023-05-11 22:25:51 -0400

    updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,46 @@
-import streamlit as st
+# import streamlit as st
+# import plotly.express as px
+# import pandas as pd
+# from streamlit_plotly_events import plotly_events
+# import numpy as np
+
+# x = [1, 2, 3, 4, 5]
+# y = [6, 7, 2, 4, 5]
+
+# df=[]
+# df= pd.DataFrame(df)
+# df['year']= x
+# df['lifeExp']= y
+
+# fig = px.line(df, x="year", y="lifeExp", title='Life expectancy in Canada')
+
+# selected_points = plotly_events(fig)
+# selected_points
+# a=selected_points[0]
+
+# df = pd.DataFrame(
+#    np.random.randn(50, 20),
+#    columns=('col %d' % i for i in range(20)))
+
+# st.dataframe(df)  # Same as st.write(df)
+# # df
+# # a= pd.DataFrame.from_dict(a,orient='index')
+# # a
+
+
+# streamlit_app.py
+
 import pandas as pd
-import numpy as np
+import streamlit as st
 
-chart_data = pd.DataFrame(
-    np.random.randn(20, 3),
-    columns=["a", "b", "c"])
+# Read in data from the Google Sheet.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+def load_data(sheets_url):
+    csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
+    return pd.read_csv(csv_url)
 
-st.bar_chart(chart_data)
+df = load_data(st.secrets["public_gsheets_url"])
+df
+# # Print results.
+# for row in df.itertuples():
+#     st.write(f"{row.name} has a :{row.pet}:")
